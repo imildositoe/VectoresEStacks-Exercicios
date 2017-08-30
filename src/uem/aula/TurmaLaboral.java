47deed9affdba20f79ca22044d403999ebf3633f
50enta <valtercinquenta@outlook.com>
2017-08-29 16:03:25 -1200

    continuação do exercício

--- src/uem/aula/TurmaLaboral.java
+++ src/uem/aula/TurmaLaboral.java
@@ -1,17 +1,25 @@
-
 package uem.aula;
+
+import java.util.Vector;
 
 /**
  *
  * @author Alfredo
  */
 public class TurmaLaboral {
+//atributos
+    Vector<Estudante> estudante;
+
+    public TurmaLaboral(int numeroEstudantes) {
+        estudante = new Vector<>(numeroEstudantes);
+    }
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+
     }
-    
+
 }
